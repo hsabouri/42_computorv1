ed1e374034122b3ee4223c04559cc1e1daae3bc3
Hugo SABOURIN <hsabouri@e3r2p22.42.fr>
2018-01-31 20:30:24 +0100

    solving non complex

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const Parser = require('./lib/parser')
 const Display = require('./lib/display')
+const Maths = require('./lib/maths')
+const error = require('./lib/error')
 
-const [equation, left, right] = Parser.getEquation()
+const equation = Parser.getEquation()
 
-Display.unreduced(left, right)
+const order = Maths.getOrder(equation)
+Display.reduced(equation)
+
+console.log(`Polynomial degree: ${order}`)
+
+if (order > 2)
+	error("Provided equation's degree is not supported")
+
+const polynome = new Maths.polynome(equation)
+
+Display.delta(polynome.getDelta())
+
+const solutions = polynome.solve()
+
+Display.solutions(solutions)
