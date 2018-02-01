9fce4adf39dcd6f74bba3da67c24d9516acb1f85
Hugo SABOURIN <hsabouri@e3r1p1.42.fr>
2018-02-01 18:46:43 +0100

    finished. TODO: Read the subject and testing

--- index.js
+++ index.js
@@ -3,20 +3,36 @@ const Display = require('./lib/display')
 const Maths = require('./lib/maths')
 const error = require('./lib/error')
 
-const equation = Parser.getEquation()
+const _static_sortByOrder = equation => {
+	let res = []
 
+	for (let i in equation) {
+		let toPush = 0
+
+		for (let token in equation) {
+			if (equation[token].order > equation[toPush].order)
+				toPush = token
+		}
+		res = res.concat(equation.splice(toPush, 1))
+	}
+	res = res.concat(equation.splice(0, 1))
+	return res
+}
+
+let equation = Parser.getEquation()
+equation = _static_sortByOrder(equation)
 const order = Maths.getOrder(equation)
+const polynome = new Maths.polynome(equation)
+
 Display.reduced(equation)
-
-console.log(`Polynomial degree: ${order}`)
-
+Display.degree(order)
 if (order > 2)
 	error("Provided equation's degree is not supported")
 
-const polynome = new Maths.polynome(equation)
-
-Display.delta(polynome.getDelta())
-
 const solutions = polynome.solve()
+
+if (order == 2) {
+	Display.delta(polynome.getDelta())
+}
 
 Display.solutions(solutions)
